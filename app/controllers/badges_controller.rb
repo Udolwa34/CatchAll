5605da143980dac278d75a832560d996ad918cb1
Dame17 <d.mali@hotmail.fr>
2015-05-06 22:02:02 +0200

    add badges by region controller method + edit badges.index view

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -5,6 +5,17 @@ class BadgesController < ApplicationController
   # GET /badges.json
   def index
     @badges = Badge.all
+    @regions = ["Kanto", "Johto", "Hoenn", "Sinnoh", "Unys", "Kalos"]
+    @badgesByRegion = [[], [], [], [], [], []]
+
+    @badges.each do |badge|
+      for n in (0..5) do
+        if badge.region == @regions[n]
+           @badgesByRegion[n].push badge
+          break
+        end
+      end
+    end 
   end
 
   # GET /badges/1
