cbb9a797e0b62b1d9ec723de19be71c3ca0b2846
PaulineCapot <pauline.capot@y-nov.com>
2015-05-11 15:08:06 +0200

    Create index MegaBookedex view

--- app/controllers/megabookedex_controller.rb
+++ app/controllers/megabookedex_controller.rb
@@ -4,6 +4,17 @@ class MegabookedexController < ApplicationController
   def index
     #@pokemons = Pokemon.all
     @badges = Badge.all
+    @regions = ["Kanto", "Johto", "Hoenn", "Sinnoh", "Unys", "Kalos"]
+    @badgesByRegion = [[], [], [], [], [], []]
+
+    @badges.each do |badge|
+      for n in (0..5) do
+        if badge.region == @regions[n]
+           @badgesByRegion[n].push badge
+          break
+        end
+      end
+    end 
   end
 
 end
