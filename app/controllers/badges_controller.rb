225a6383313fe1047807cfade21237363b31a73b
Udolwa34 <ludovic.caisso@y-nov.com>
2015-05-11 19:20:41 +0200

    Add authentification verification on badges/pokemon controllers

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,5 +1,6 @@
 class BadgesController < ApplicationController
   before_action :set_badge, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_trainer!
 
   # GET /badges
   # GET /badges.json
