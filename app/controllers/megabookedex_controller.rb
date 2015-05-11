eebcc017b53ef8ac506d3119e435c4c8503aea3a
Udolwa34 <ludovic.caisso@y-nov.com>
2015-05-11 19:23:05 +0200

    Add <dev-only> methods for Trainers delete/sign out

--- app/controllers/megabookedex_controller.rb
+++ app/controllers/megabookedex_controller.rb
@@ -6,4 +6,17 @@ class MegabookedexController < ApplicationController
     @badges = Badge.all
   end
 
+  # DELETE /trainers
+  # DEV method only
+  def deltrainers
+  	@trainers = Trainer.all
+  	@trainers.each do |trainer|
+  		trainer.destroy
+  	end
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: 'Trainers were successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 end
