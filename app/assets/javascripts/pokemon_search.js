047d961f8a8ab233ac3a5dd45e77d3352f6d980b
Udolwa34 <ludovic.caisso@y-nov.com>
2015-05-12 12:01:03 +0200

    Add buttons Next-Prev on Pokemon Search

--- app/assets/javascripts/pokemon_search.js
+++ app/assets/javascripts/pokemon_search.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+	numeroPokemon = 0;
     //Function that handle autocompletion
 	$(function () {
 	    $('#search').autocomplete({
@@ -40,12 +40,25 @@ $(document).ready(function(){
 
 	});
 
+
+	//Next & Prev buttons
+	$("#nextButton").click(function(){
+		searchPkmn(numeroPokemon + 1);
+	});
+	$("#prevButton").click(function(){
+		searchPkmn(numeroPokemon - 1);
+	});
+
 });
 
 
 //Function that handle search of the Pokemon's informations on Pokeapi
 //Parameter : numPokemon => national_id of the Pokemon
 function searchPkmn(numPokemon){
+	//Hides buttons "Next" and "Prev"
+	$("#prevButton").attr("disabled", false);
+	$("#nextButton").attr("disabled", false);
+
 	$("#result").text("Wait please...");
 	$.ajax({
 	       url: "http://pokeapi.co/api/v1/pokemon/"+numPokemon+"",
@@ -55,9 +68,18 @@ function searchPkmn(numPokemon){
 		   },
 		   
 		   success: function(data) {
+		   		numeroPokemon = parseInt(numPokemon);
 
 		   		$("#result").text(data.name + "--" + data.national_id + "--"+ data.types[0].name+"-- + a lot of more informations !");
 
+		   		//Shows, or not, buttons "Next" and "Prev".
+		   		if ( numPokemon == 1){
+					$("#prevButton").attr("disabled", true);
+					$("#nextButton").attr("disabled", false);	
+				} else if ( numPokemon == $("#pokemonMax").text() ){
+					$("#prevButton").attr("disabled", false);
+					$("#nextButton").attr("disabled", true);
+				}
 		   },
 		   
 	});
