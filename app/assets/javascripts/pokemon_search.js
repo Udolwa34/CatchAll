87d2c7f27208907d08e2d9288f49f8f246c3851d
GillianPerard <gillian.perard@gmail.com>
2015-05-12 20:32:27 +0200

    Add 0.png to Pokemon img + modification search.html.erb

--- app/assets/javascripts/pokemon_search.js
+++ app/assets/javascripts/pokemon_search.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+	
 	numeroPokemon = 0;
+
     //Function that handle autocompletion
 	$(function () {
 	    $('#search').autocomplete({
@@ -55,7 +57,6 @@ $(document).ready(function(){
 
 });
 
-
 //Function that handle search of the Pokemon's informations on Pokeapi
 //Parameter : numPokemon => national_id of the Pokemon
 function searchPkmn(numPokemon){
@@ -63,32 +64,60 @@ function searchPkmn(numPokemon){
 	$("#prevButton").attr("disabled", false);
 	$("#nextButton").attr("disabled", false);
 
-	$("#result").text("Wait please...");
 	$.ajax({
 	       url: "http://pokeapi.co/api/v1/pokemon/"+numPokemon+"",
 	       type: 'GET',
 		   error: function() {
-		      alert('An error occured during the operation. Please, try again later.');
+		      	//alert('An error occured during the operation. Please, try again later.');
+		      	$("#pokemonImage").attr("src", "/assets/pokemon/0.png");
+		   		$("#pokemonName h1").text("No result");
+		   		$("#tdName").text("-");
+		   		$("#tdId").text("-");
+		   		$("#tdHeight").text("-");
+		   		$("#tdWeight").text("-");
+		   		$("#tdAttackDefense").text("-");
+		   		$("#tdType1").text("-");
+		   		$("#tdType2").text("-");
+		   		$("#tdSpecies").text("-");
+		   		$("#tdEvolution").text("-");
+
+		   		$("#search").val("");
 		   },
 		   
 		   success: function(data) {
 		   		numeroPokemon = parseInt(numPokemon);
-
+		   		console.log(data)
 		   		$("#pokemonImage").attr("src", "/assets/pokemon/"+data.national_id+".png");
 		   		$("#pokemonName h1").text(data.name);
 		   		$("#tdName").text(data.name);
 		   		$("#tdId").text(data.national_id);
+		   		$("#tdHeight").text(data.height/10 + "m");
+		   		$("#tdWeight").text(data.weight/10 + "kg");
+		   		$("#tdAttackDefense").text(data.attack + " / " + data.defense);
 		   		$("#tdType1").text(data.types[0].name);
 		   		data.types.length > 1 ? $("#tdType2").text(data.types[1].name) : $("#tdType2").text("None");
+		   		var str = "";
+		   		for (var i = 0; i < data.egg_groups.length; i++) {
+		   			str += data.egg_groups[i].name;
+		   			if(i != data.egg_groups.length - 1 && data.egg_groups.length > 1 ) str += ", ";
+		   		};
+		   		$("#tdSpecies").text(str != "" ? str : "None");
+		   		for (var i = 0; i < data.evolutions.length; i++) {
+		   			if(i == 0) $("#tdEvolution").text("");
+		   			$("#tdEvolution").append("<a onclick='searchPkmn(" + data.evolutions[i].resource_uri.split("/")[4] + ")'>" + data.evolutions[i].to + " (" + data.evolutions[i].level + ")</a>");
+		   			if(i != data.evolutions.length - 1 && data.evolutions.length > 1)  $("#tdEvolution").append(", ");;
+		   		};
+		   		if(data.evolutions.length == 0) $("#tdEvolution").text("None");
 
 		   		//Shows, or not, buttons "Next" and "Prev".
-		   		if ( numPokemon == 1){
+		   		if (numPokemon == 1){
 					$("#prevButton").attr("disabled", true);
 					$("#nextButton").attr("disabled", false);	
 				} else if ( numPokemon == $("#pokemonMax").text() ){
 					$("#prevButton").attr("disabled", false);
 					$("#nextButton").attr("disabled", true);
 				}
+				$("#search").val(data.name + " - #" + data.national_id);
 		   },
 		   
 	});
