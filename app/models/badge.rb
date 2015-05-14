016d6ec48d3c7ecba6602575ee3ed30b18d585b0
Udolwa34 <ludovic.caisso@y-nov.com>
2015-05-14 20:23:57 +0200

    Add association rules on Badge & Trainer models

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,3 @@
 class Badge < ActiveRecord::Base
+	has_and_belongs_to_many :trainers, :join_table => 'trainers_badges'
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,6 +2,8 @@ class Trainer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable
          :confirmable
+
+  has_and_belongs_to_many :badges, :join_table => 'trainers_badges'
 end
