0f5e3c8047425b87f61ffe4c86a77e5c16d8aa2d
Udolwa34 <ludovic.caisso@y-nov.com>
2015-05-14 20:34:20 +0200

    Add JS method removeBadgeFromTrainer()

--- app/assets/javascripts/megabookedex_badges.js
+++ app/assets/javascripts/megabookedex_badges.js
@@ -1,9 +1,39 @@
 //Removing a badge from a trainer's collection
 function removeBadgeById(idBadge){
-	
+	$.ajax({
+       url: "/badges/removeFromTrainer/"+idBadge,
+       type: 'GET',
+       dataType: 'json',
+       success: function (data) {
+       	 //Managing display of row's buttons and informations
+       	 badgeRow = $("#badgeRow"+idBadge);
+		 badgeRow.find(".badgeObtainedColumn").text("No");
+		 badgeRow.find(".removeBadgeButton").hide();
+		 badgeRow.find(".addBadgeButton").show();
+
+		 //Managing progression-bar 
+		 updateBadgeProgressBar(data["total"], data["trainerNbr"]);
+        
+       },
+       error : function (){
+       	alert('An error occured during the operation. Please, try again later.');
+       }
+   });
 }
 
-//Adding a badge to a trainer's collection
-function addBadgeById(idBadge){
-	
-}
+
+
+//Managing progression-bar 
+function updateBadgeProgressBar(badgesTotal, trainerBadgesCount){
+	 prctCompletion = ((trainerBadgesCount * 100)/ badgesTotal);
+	 $("#badgeProgressBar").attr("aria-valuenow", prctCompletion);
+	 $("#badgeProgressBar").css("width", prctCompletion+"%");
+
+	 $("#totalPrctCompletion").text(prctCompletion+"% Complete");
+	 $("#totalBadges").text(trainerBadgesCount+"/"+badgesTotal);
+	 if ( prctCompletion > 7 ){
+	 	$("#totalBadges").show();
+	 } else {
+	 	$("#totalBadges").hide();
+	 }
+} 
