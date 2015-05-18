fc4202224e458ff6499843fb5d8bfc1111f03ce0
Dame17 <d.mali@hotmail.fr>
2015-05-18 14:19:35 +0200

    migration add link between pokemon and trainers

---
+++ db/migrate/20150518120834_add_link_between_pokemon_and_trainers.rb
@@ -0,0 +1,9 @@
+class AddLinkBetweenPokemonAndTrainers < ActiveRecord::Migration
+  def change
+  	create_table :huntstates do |t|
+  		t.belongs_to :pokemon, index: true
+  		t.belongs_to :trainer, index: true
+  		t.integer :viewed
+  		t.integer :caught
+  end
+end
