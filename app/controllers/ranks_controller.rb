780d2f19c86aea96ea89afbe9f9888de5eb50386
GillianPerard <gillian.perard@gmail.com>
2015-05-23 20:23:25 +0200

    Modification ranking page with rank values

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -4,19 +4,22 @@ class RanksController < ApplicationController
 		# Pokemon
 		@pokemonsAll = Pokemon.all
         @pokemonMax = @pokemonsAll.count
-        @trainerPokemonCaughtCount = (current_trainer.pokemons).where('huntstates.caught = 1').count
-        @trainerPokemonSeenCount = (current_trainer.pokemons).count
 
         # Badge
         @badgesAll = Badge.all
         @badgeMax = @badgesAll.count
-  		@trainerBadgesCount = (current_trainer.badges).count
 
   		# Trainer
 		@trainersAll = Trainer.all
 		@trainerMax = @trainersAll.count
 		@trainerPseudo = current_trainer.email
 		@trainers = @trainersAll.limit(50).order("id asc")
+
+		@ranksAll = Rank.all
+		@ranks = @ranksAll.limit(50).order("total_points desc")
+		@trainerRank = current_trainer.rank
+
+
 	end
 
 	#Dev route
