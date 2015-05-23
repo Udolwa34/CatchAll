f85ffa80f78bcb1bf9de5304cdee9e2148e485c5
Dame17 <d.mali@hotmail.fr>
2015-05-23 10:21:00 +0200

    update route ranking + update link ranking controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'contact_us' => 'contacts#new'
-  get 'ranking' => 'rankings#index'
+  get 'ranking' => 'ranks#index'
 
   get 'account' => 'trainers#show'
 
